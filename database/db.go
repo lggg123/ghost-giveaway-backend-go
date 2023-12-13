a4225a1451e977485e2287c104b184f68c2c49b2
lggg123 <machinelearner2334@gmail.com>
2023-12-13 11:21:04 -0800

    added model

--- database/db.go
+++ database/db.go
@@ -2,43 +2,72 @@
 package database
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 
 	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-var db *sql.DB
+var DB *gorm.DB
 
-const (
-	host   = "localhost"
-	port   = 5432
-	user   = "postgres"
-	dbname = "ghosterz"
-)
-
+// InitDB initializes the database connection
 func InitDB() {
-	connStr := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable", host, port, user, dbname)
+
+	host := "postgres"
+	port := 5432
+	user := "postgres"
+	password := "password"
+	dbname := "ghosterz"
+	// Connection string
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
 	var err error
-	db, err = sql.Open("postgres", connStr)
+	// Connect to the database
+	DB, err = gorm.Open(postgres.Open(connStr), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = db.Ping()
+	// Check the connection
+	sqlDB, err := DB.DB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = sqlDB.Ping()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println("Connected to the PostgreSQL database")
+	log.Println("Connected to the database")
 }
 
+// CloseDB closes the database connection
 func CloseDB() {
-	db.Close()
+	sqlDB, err := DB.DB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	sqlDB.Close()
 }
 
+// MigrateDB performs database migrations
 func MigrateDB() {
-	// Implement your migration logic here
-	// Example: db.Exec("CREATE TABLE IF NOT EXISTS users (id serial primary key, username varchar, email varchar);")
+	// Perform your database migrations here
+	_, err := DB.Exec(`
+		CREATE TABLE users (
+			id SERIAL PRIMARY KEY,
+			username VARCHAR(255),
+			email VARCHAR(255),
+			password VARCHAR(255)
+		);
+	`)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Database migration completed")
 }
--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/lggg123/ghost-giveaway-backend-go/database" // Update with the correct module path
-
+	"github.com/lggg123/ghost-giveaway-backend-go/database"
+	// Update with the correct module path
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 )
