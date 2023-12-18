a98e519fcd39f9c456f7eadcb5564d892e2aa580
lggg123 <machinelearner2334@gmail.com>
2023-12-17 19:43:25 -0800

    added optionsHandler for heroku deployment

--- main.go
+++ main.go
@@ -18,9 +18,14 @@ func main() {
 	database.MigrateDB()
 
 	router := mux.NewRouter()
+	router.Methods(http.MethodOptions).HandlerFunc(optionsHandler).MatcherFunc(func(r *http.Request, rm *mux.RouteMatch) bool {
+		return r.URL.Path == "/signup"
+	})
 	router.HandleFunc("/signup", signupHandler).Methods("POST")
 
 	corsHandler := cors.Default().Handler(router)
+
+	http.Handle("/", corsHandler)
 
 	// Determine the port from the environment variable or default to 8080
 	port := os.Getenv("PORT")
@@ -32,4 +37,13 @@ func main() {
 
 	log.Printf("Server is running on http://localhost:%s\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), corsHandler))
+}
+
+func optionsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	// Respond with 200 OK for OPTIONS requests
+	w.WriteHeader(http.StatusOK)
 }
